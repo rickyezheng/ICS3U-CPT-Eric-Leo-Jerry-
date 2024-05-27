13e43e9797f9436bf714e83993a8246de2b1c0d9
Jerry <zirui.wang26@ycdsbk12.ca>
2024-05-27 11:46:20 -0400

    Changed main to match enemy.py added extra files

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+from enemy import Enemy
 import constants
 
 #initialise pygame
@@ -9,7 +10,16 @@ clock = pygame.time.Clock()
 
 #create game window
 screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
-pygame.display.set_caption(" WYZTower Defence")
+pygame.display.set_caption("Tower Defence")
+
+#load images
+enemy_image = pygame.image.load('assets/enemy_1.png').convert_alpha()
+
+#create groups
+enemy_group = pygame.sprite.Group()
+
+enemy = Enemy((200, 300), enemy_image)
+enemy_group.add(enemy)
 
 #game loop
 run = True
@@ -17,10 +27,21 @@ while run:
 
   clock.tick(constants.FPS)
 
+  screen.fill("grey100")
+
+  #update groups
+  enemy_group.update()
+
+  #draw groups
+  enemy_group.draw(screen)
+
   #event handler
   for event in pygame.event.get():
     #quit program
     if event.type == pygame.QUIT:
       run = False
+
+  #update display
+  pygame.display.flip()
 
 pygame.quit()
