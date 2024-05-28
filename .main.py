abe941fbf9bb7d1b702e1d58b8ba512770e2c4a4
leoyang812 <leoy812@gmail.com>
2024-05-28 14:28:12 -0400

    Created cancel/buy turret and turret

--- .main.py
+++ .main.py
@@ -3,26 +3,31 @@ import json
 from enemy import Enemy
 from world import World
 from turret import Turret
+from button import Button
 import constants
+
 
 #initialise pygame
 pygame.init()
 
-#create clock
+#creates clock
 clock = pygame.time.Clock()
 
-#create game window
-screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+#creates game window
+screen = pygame.display.set_mode((constants.SCREEN_WIDTH +constants.SIDE_PANEL  , constants.SCREEN_HEIGHT))
 pygame.display.set_caption("Tower Defence")
-
+#game variables
+placing_turrets= False
 #load images
 #map
 map_image = pygame.image.load('assets/level.png').convert_alpha()
 #individual turret image for mouse cursor
 cursor_turret = pygame.image.load('assets/cursor_turret.png').convert_alpha()
-#enemies
+#enemies- Icon from online
 enemy_image = pygame.image.load('assets/enemy_1.png').convert_alpha()
-
+buy_turret_image= pygame.image.load("assets/buy_turret.png").convert_alpha()
+#cancel image
+cancel_image=pygame.image.load('assets/cancel.png').convert_alpha()
 #load json data for level
 with open('assets/level.tmj') as file:
   world_data = json.load(file)
@@ -55,23 +60,35 @@ turret_group = pygame.sprite.Group()
 enemy = Enemy(world.waypoints, enemy_image)
 enemy_group.add(enemy)
 
+
+#Buttons
+turret_button = Button(constants.SCREEN_WIDTH + 30, 120, cursor_turret)
+cancel_button = Button(constants.SCREEN_WIDTH + 50, 180, cancel_image) 
 #game loop
 run = True
 while run:
 
   clock.tick(constants.FPS)
+#updating
 
+  enemy_group.update() 
+#drawing section
   screen.fill("grey100")
 
   #draw level
   world.draw(screen)
 
-  #update groups
-  enemy_group.update()
 
   #draw groups
   enemy_group.draw(screen)
   turret_group.draw(screen)
+
+  #draw buttons
+  #turret placing
+  if turret_button.draw(screen):
+    print("new turret")
+  if cancel_button.draw(screen):
+    print("cancel")
 
   #event handler
   for event in pygame.event.get():
