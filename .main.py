8a2b3eccb29dcc9161be0623278c53eb3a7e3987
J Wang <159564331+J-WCS@users.noreply.github.com>
2024-05-31 11:54:31 -0400

    Update .main.py

--- .main.py
+++ .main.py
@@ -36,35 +36,35 @@ cancel_image = pygame.image.load('assets/cancel.png').convert_alpha()
 
 #load json data for level
 with open('assets/level.tmj') as file:
-  world_data = json.load(file)
+    world_data = json.load(file)
 
 def create_turret(mouse_pos):
-  mouse_tile_x = mouse_pos[0] // constants.TILE_SIZE
-  mouse_tile_y = mouse_pos[1] // constants.TILE_SIZE
-  #calculate the sequential number of the tile
-  mouse_tile_num = (mouse_tile_y * constants.COLS) + mouse_tile_x
-  #check if that tile is grass
-  if world.tile_map[mouse_tile_num] == 7:
-    #check that there isn't already a turret there
-    space_is_free = True
-    for turret in turret_group:
-      if (mouse_tile_x, mouse_tile_y) == (turret.tile_x, turret.tile_y):
-        space_is_free = False
-    #if it is a free space then create turret
-    if space_is_free == True:
-      new_turret = Turret(turret_sheet, mouse_tile_x, mouse_tile_y)
-      turret_group.add(new_turret)
+    mouse_tile_x = mouse_pos[0] // constants.TILE_SIZE
+    mouse_tile_y = mouse_pos[1] // constants.TILE_SIZE
+    #calculate the sequential number of the tile
+    mouse_tile_num = (mouse_tile_y * constants.COLS) + mouse_tile_x
+    #check if that tile is grass
+    if world.tile_map[mouse_tile_num] == 7:
+        #check that there isn't already a turret there
+        space_is_free = True
+        for turret in turret_group:
+            if (mouse_tile_x, mouse_tile_y) == (turret.tile_x, turret.tile_y):
+                space_is_free = False
+        #if it is a free space then create turret
+        if space_is_free == True:
+            new_turret = Turret(turret_sheet, mouse_tile_x, mouse_tile_y)
+            turret_group.add(new_turret)
 
 def select_turret(mouse_pos):
-  mouse_tile_x = mouse_pos[0] // constants.TILE_SIZE
-  mouse_tile_y = mouse_pos[1] // constants.TILE_SIZE
-  for turret in turret_group:
-    if (mouse_tile_x, mouse_tile_y) == (turret.tile_x, turret.tile_y):
-      return turret
+    mouse_tile_x = mouse_pos[0] // constants.TILE_SIZE
+    mouse_tile_y = mouse_pos[1] // constants.TILE_SIZE
+    for turret in turret_group:
+        if (mouse_tile_x, mouse_tile_y) == (turret.tile_x, turret.tile_y):
+            return turret
 
 def clear_selection():
-  for turret in turret_group:
-    turret.selected = False
+    for turret in turret_group:
+        turret.selected = False
 
 #create world
 world = World(world_data, map_image)
@@ -82,80 +82,71 @@ turret_button = Button(constants.SCREEN_WIDTH + 30, 120, buy_turret_image, True)
 cancel_button = Button(constants.SCREEN_WIDTH + 50, 180, cancel_image, True)
 
 #initialize main menu
-main_menu = MainMenu(screen)  # Added initialization
-state = 'main_menu'  # Added state variable
+main_menu = MainMenu(screen)
+state = 'main_menu'
 
 #game loop
 run = True
 while run:
-  clock.tick(constants.FPS)
+    clock.tick(constants.FPS)
 
-  if state == 'main_menu':  # Added main menu state handling
-    state = main_menu.draw()
-  elif state == 'start':  # Game starts when 'start' state is active
-  #########################
-  # UPDATING SECTION
-  #########################
+    if state == 'main_menu':
+        state = main_menu.draw()
+    elif state == 'start':
+        #########################
+        # UPDATING SECTION
+        #########################
 
-  #update groups
-    enemy_group.update()
-    turret_group.update(enemy_group)
+        #update groups
+        enemy_group.update()
+        turret_group.update(enemy_group)
 
-    #highlight selected turret
-    if selected_turret:
-      selected_turret.selected = True
+        #highlight selected turret
+        if selected_turret:
+            selected_turret.selected = True
 
-    #########################
-    # DRAWING SECTION
-    #########################
+        #########################
+        # DRAWING SECTION
+        #########################
 
-    screen.fill("grey100")
+        screen.fill("grey100")
 
-    #draw level
-    world.draw(screen)
+        #draw level
+        world.draw(screen)
 
-    #draw groups
-    enemy_group.draw(screen)
-    for turret in turret_group:
-      turret.draw(screen)
+        #draw groups
+        enemy_group.draw(screen)
+        for turret in turret_group:
+            turret.draw(screen)
 
-    #draw buttons
-    #button for placing turrets
-    if turret_button.draw(screen):
-      placing_turrets = True
-    #if placing turrets then show the cancel button as well
-    if placing_turrets == True:
-      #show cursor turret
-      cursor_rect = cursor_turret.get_rect()
-      cursor_pos = pygame.mouse.get_pos()
-      cursor_rect.center = cursor_pos
-      if cursor_pos[0] <= constants.SCREEN_WIDTH:
-        screen.blit(cursor_turret, cursor_rect)
-      if cancel_button.draw(screen):
-        placing_turrets = False
+        #draw buttons
+        if turret_button.draw(screen):
+            placing_turrets = True
+        if placing_turrets == True:
+            cursor_rect = cursor_turret.get_rect()
+            cursor_pos = pygame.mouse.get_pos()
+            cursor_rect.center = cursor_pos
+            if cursor_pos[0] <= constants.SCREEN_WIDTH:
+                screen.blit(cursor_turret, cursor_rect)
+            if cancel_button.draw(screen):
+                placing_turrets = False
 
     #event handler
     for event in pygame.event.get():
-      #quit program
-      if event.type == pygame.QUIT:
-        run = False
-      #mouse click
-      if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
-        mouse_pos = pygame.mouse.get_pos()
-        #check if mouse is on the game area
-        if mouse_pos[0] < constants.SCREEN_WIDTH and mouse_pos[1] < constants.SCREEN_HEIGHT:
-          #clear selected turrets
-          selected_turret = None
-          clear_selection()
-          if placing_turrets == True:
-            create_turret(mouse_pos)
-          else:
-            selected_turret = select_turret(mouse_pos)
+        if event.type == pygame.QUIT:
+            run = False
+        if state == 'start' and event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+            mouse_pos = pygame.mouse.get_pos()
+            if mouse_pos[0] < constants.SCREEN_WIDTH and mouse_pos[1] < constants.SCREEN_HEIGHT:
+                selected_turret = None
+                clear_selection()
+                if placing_turrets:
+                    create_turret(mouse_pos)
+                else:
+                    selected_turret = select_turret(mouse_pos)
+        if state == 'exit':
+            run = False
 
-      elif state == 'exit':  # Exit state handling
-          run = False
-
-  #update display
-  pygame.display.flip()
+    pygame.display.flip()
 
 pygame.quit()
