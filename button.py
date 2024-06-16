3db3ce7f7472fa1b19f9a6de1fe654f6716f3e29
leoyang812 <leo.yang26@ycdsbk12.ca>
2024-06-16 14:31:11 -0400

    Commented some

--- button.py
+++ button.py
@@ -1,6 +1,11 @@
 import pygame
 
 #Defining a button class that can be used to detect mouse clicks
+#1. __init__ initializes when you create a new instance of Button 
+#2 function defines x,y(coordinates that determine where it will be placed), image represents the button, single_click only allows for a single click
+#self.click initializes a flag to track whether it has been clicked
+#self.rect.topleft sets the coordinates
+#self.rect creates a pygame.rect
 class Button():
   def __init__(self, x, y, image, single_click):
     self.image = image
@@ -9,6 +14,10 @@ class Button():
     self.clicked = False
     self.single_click = single_click
 
+#1 function draws the button on specificed surface
+#2 pygame.mouse.get_pos() gets the current position- built in function
+#3 self.rect.collidepoint checks if its within the button
+#pygame. get_pressed()[] checks if its being pressed
   def draw(self, surface):
     action = False
     # Get mouse position
