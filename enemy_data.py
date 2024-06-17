41a000845e733b9edc0c5553d5759edc21df156d
Jerry <zirui.wang26@ycdsbk12.ca>
2024-06-17 18:14:23 -0400

    Balance changes

--- enemy_data.py
+++ enemy_data.py
@@ -22,50 +22,50 @@ ENEMY_SPAWN_DATA = [
   },
   {
     #4
-    "weak": 30,
+    "weak": 25,
     "medium": 15,
     "strong": 0,
     "elite": 0
   },
   {
     #5
-    "weak": 10,
+    "weak": 20,
     "medium": 20,
     "strong": 0,
     "elite": 0
   },
   {
     #6
-    "weak": 15,
-    "medium": 15,
+    "weak": 10,
+    "medium": 10,
     "strong": 5,
     "elite": 0
   },
   {
     #7
-    "weak": 25,
+    "weak": 20,
     "medium": 20,
     "strong": 5,
     "elite": 0
   },
   {
     #8
-    "weak": 10,
+    "weak": 15,
+    "medium": 20,
+    "strong": 10,
+    "elite": 0
+  },
+  {
+    #9
+    "weak": 20,
     "medium": 20,
     "strong": 15,
     "elite": 0
   },
   {
-    #9
-    "weak": 15,
-    "medium": 10,
-    "strong": 15,
-    "elite": 0
-  },
-  {
     #10
-    "weak": 0,
-    "medium": 50,
+    "weak": 10,
+    "medium": 40,
     "strong": 10,
     "elite": 5
   },
@@ -93,16 +93,16 @@ ENEMY_SPAWN_DATA = [
   {
     #14
     "weak": 0,
-    "medium": 0,
-    "strong": 30,
-    "elite": 28
+    "medium": 20,
+    "strong": 20,
+    "elite": 20
   },
   {
     #15
-    "weak": 0,
-    "medium": 0,
-    "strong": 0,
-    "elite": 50
+    "weak": 10,
+    "medium": 20,
+    "strong": 30,
+    "elite": 20
   }
 ]
 
